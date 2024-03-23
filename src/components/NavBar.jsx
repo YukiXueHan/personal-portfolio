15ba664b8472095bd9c3ef52b0f3d6ac150f803c
Xue Han <xue.b.han@outlook.com>
2024-03-23 16:23:47 -0700

    edit some page

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -50,7 +50,7 @@ const NavBar = () => {
             </Nav>
             <span className="navbar-text">
               <div className="social-icon">
-                <a href="#"><img src={navIcon1} alt="" /></a>
+                <a href="https://www.linkedin.com/in/xuehansherry/"><img src={navIcon1} alt="" /></a>
                 <a href="#"><img src={navIcon2} alt="" /></a>
                 <a href="#"><img src={navIcon3} alt="" /></a>
               </div>
